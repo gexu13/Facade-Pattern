17a69babfb5596898ff52909268ee8feabd8ad93
cocoblip <zihaoli17@yahoo.ca>
2023-04-14 15:22:18 -0700

    shape interface

--- src/Shape/Shape.java
+++ src/Shape/Shape.java
@@ -1,7 +1,25 @@
 package Shape;
 
 /**
- * Shape interface.
+ * This interface contains all operations that all types of shapes should support.
  */
 public interface Shape {
+  /**
+   * Draws a shape.
+   */
+  void draw();
+
+  /**
+   * Computes and returns the perimeter of this shape.
+   *
+   * @return the perimeter of this shape
+   */
+  int calculatePerimeter();
+
+  /**
+   * Computes and returns the area of this shape.
+   *
+   * @return  the area of this shape
+   */
+  double calculateArea():
 }
