341a8fe72538241dc5ef5a9cd452626056958ad5
cocoblip <zihaoli17@yahoo.ca>
2023-04-14 17:48:20 -0700

    add JavaDoc to Demo

--- src/Shape/Demo.java
+++ src/Shape/Demo.java
@@ -1,6 +1,17 @@
 package Shape;
 
+/**
+ * This is a demo class that interacts with ShapeManager interface.
+ */
+
 public class Demo {
+
+  /**
+   * Run a ShapeManager facade.
+   *
+   * @param args not used
+   */
+
   public static void main(String[] args) {
     ShapeManager shapeManager = new ShapeManager();
 
